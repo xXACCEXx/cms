4c05d3d8d348ff4b16bd79ea1695411df37a4e82
Lawrence Tranter <lawrence.tranter@gmail.com>
2018-03-04 10:57:23 +1100

    added all dem changes

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -2,11 +2,16 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const prefix = require('gulp-autoprefixer')
 const cssnano = require('gulp-cssnano')
+const source = require('vinyl-source-stream')
 
 gulp.task('sass', () => {
-	gulp.src('./src/sass/*.scss')
+	gulp.src([
+		'./src/sass/*.scss',
+		'./src/components/**/style.scss'
+	])
 		.pipe(sass({ style: 'expanded', includePaths: ['./node_modules/bulma', './src/icons'] }))
 		.pipe(prefix('last 2 versions'))
 		.pipe(cssnano())
+		.pipe(source('main.css'))
 		.pipe(gulp.dest('./public/css'))
 })
